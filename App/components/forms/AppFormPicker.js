0c2cd4fb97c7b678d64f0563189c9d5e71408cb7
Rama <sadamramanayudu@gmail.com>
2021-09-23 20:00:05 +1000

    New Component to display categories

---
+++ App/components/forms/AppFormPicker.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { useFormikContext } from 'formik';
+
+import AppPicker from '../AppPicker';
+import ErrorMessage from '../forms/ErrorMessage'
+import colors from '../config/colors';
+
+function AppFormPicker({items, name, placeholder }) {
+    const { errors, setFieldValue, touched, values } = useFormikContext();
+    return (
+        <>
+            <AppPicker
+                        items={items}
+                        onSelectItem={(item) => setFieldValue(name, item)}
+                        placeholder={placeholder}
+                        selectedItem={values[name]}
+            />
+            <ErrorMessage error={errors[name]} visible={touched[name]} /> 
+        
+        </>
+    ); 
+}
+
+export default AppFormPicker;
