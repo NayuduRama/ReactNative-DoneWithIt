15dc0b31fc6f731738c70dc6321bf30d3a51f524
Rama <sadamramanayudu@gmail.com>
2021-09-23 20:14:50 +1000

    Screen to add new items

---
+++ App/Screens/ListingEditScreen.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import * as Yup from 'yup'
+
+import { AppForm, AppFormField, SubmitButton } from '../components/forms';
+import AppFormPicker from '../components/forms/AppFormPicker';
+import Screen from './Screen';
+
+const validationSchema = Yup.object().shape({
+    title: Yup.string().required().min(1).label("Title"),
+    price: Yup.string().required().max(100000).label("Price"),
+    description: Yup.string().label("Description"),
+    category: Yup.object().required().nullable().label("Category")
+
+})
+
+const categories =[
+    {label:"Furniture", value:'1'},
+    {label:"Clothing", value:'2'},
+    {label:"Electronic", value:'3'}
+
+]
+
+function ListingEditScreen() {
+    return (
+         <Screen>
+             <AppForm
+                initialValues={{
+                                    title:"",
+                                    price:"",
+                                    category:null,
+                                    description:"" } }
+                onSubmit={values => console.log(values)}
+                validationSchema={validationSchema}
+                >
+                 <AppFormField
+                                name="title"
+                                placeholder="Title"
+                                />
+                <AppFormField
+                                name="price"
+                                keyboardType="numeric"
+                                maxLength={8}
+                                placeholder="Price"
+                                />
+                <AppFormPicker
+                                items={categories}
+                                name="category"
+                                placeholder="Category"
+                                />
+                <AppFormField
+                                name="description"
+                                maxLength={255}
+                                multiline
+                                numberOfLines={3}
+                                placeholder="Description"
+                                />
+                <SubmitButton title="Post"/>
+             </AppForm>
+         </Screen>
+    );
+}
+
+export default ListingEditScreen;
